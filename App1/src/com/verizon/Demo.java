2d5dc35ed32f35b0cc3a7da7d3834ce962c21fc5
Shanmukhasai908 <88928735+Shanmukhasai908@users.noreply.github.com>
2024-08-29 10:48:39 +0530

    Update Demo.java

--- App1/src/com/verizon/Demo.java
+++ App1/src/com/verizon/Demo.java
@@ -4,9 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Java !");
-		System.out.println(args[0]);
-		System.out.println(args[1]);
+		System.out.println("Welcome to Jenlins !");
+	
 
 	}
 
